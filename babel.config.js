db142f208595585d5ae246f2b16e2594e0fb51b5
Technote <technote.space@gmail.com>
2020-08-02 21:22:15 +0900

    chore: tweaks

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,24 @@
 module.exports = {
-    presets: [
-        [
-            '@babel/preset-env',
-            {
-                targets: {
-                    node: 'current',
-                },
-            },
-        ],
-        '@babel/preset-flow',
-        '@babel/preset-react',
-        '@babel/typescript',
-    ],
-    plugins: [
-        '@babel/plugin-proposal-class-properties',
-        '@babel/proposal-object-rest-spread',
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-transform-react-inline-elements',
-        'react-html-attrs',
-        'transform-class-properties',
-        [
-            'i18next-extract', {
-            locales: ['ja', 'en'],
-            outputPath: './src/locales/{{locale}}.json',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
         },
-        ],
+      },
     ],
-    ignore: ['node_modules', 'build', 'distDev', 'distProd'],
+    '@babel/preset-flow',
+    '@babel/preset-react',
+    '@babel/typescript',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-react-inline-elements',
+    'react-html-attrs',
+    'transform-class-properties',
+  ],
+  ignore: ['node_modules', 'build', 'distDev', 'distProd'],
 };
